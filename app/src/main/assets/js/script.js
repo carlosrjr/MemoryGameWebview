d0d1c086362d1b9effcbd2cbd19d3ff37861ee1e
Carlos Roberto Barbosa Junior <carlosjr.if@gmail.com>
2021-03-19 14:29:47 -0300

    [ADD] Tela de fim de jogo

--- app/src/main/assets/js/script.js
+++ app/src/main/assets/js/script.js
@@ -1,14 +1,19 @@
-let btn1 = document.getElementById("1");
-let btn2 = document.getElementById("2");
-let btn3 = document.getElementById("3");
-let btn4 = document.getElementById("4");
-let btn5 = document.getElementById("5");
-let btn6 = document.getElementById("6");
-let score = document.getElementById("score");
-let body = document.getElementById("body");
-let btn_reset = document.getElementById("reset");
+const btn1 = document.getElementById("1");
+const btn2 = document.getElementById("2");
+const btn3 = document.getElementById("3");
+const btn4 = document.getElementById("4");
+const btn5 = document.getElementById("5");
+const btn6 = document.getElementById("6");
+const score = document.getElementById("score");
+const body = document.getElementById("body");
+const btn_reset = document.getElementById("reset");
+const progress = document.getElementById("progress");
+const game = document.getElementById("game");
+const end_message = document.getElementById("end_message");
+const errors = document.getElementById("errors");
 
 let started = false;
+let end = false;
 
 let timer;
 
@@ -25,9 +30,10 @@ const WHITE = "#ffffff";
 
 const sequence = [1,2,3,4,5,6];
 let selected = 0;
+let count_errors = 0;
 
 function init() {
-    stopTimer();
+    setEndGame();
     btn1.style.backgroundColor = BLUE;
     btn2.style.backgroundColor = RED;
     btn3.style.backgroundColor = GREEN;
@@ -39,7 +45,12 @@ function init() {
     points = TIME_GAME;
     btn_reset.textContent = "Iniciar";
     started = false;
+    end = false;
     selected=0;
+    count_errors=0;
+    progress.style.width = "100%"
+    errors.textContent = "Erros: 0";
+    changeContent();
     visibleAll();
 }
 
@@ -56,37 +67,39 @@ function startGame() {
 }
 
 function checkEndgame() {
-    return btn1.classList.contains("hide") &&
-        btn2.classList.contains("hide") &&
-        btn3.classList.contains("hide") &&
-        btn4.classList.contains("hide") &&
-        btn5.classList.contains("hide") &&
-        btn6.classList.contains("hide");
+    return btn1.classList.contains("invisible") &&
+        btn2.classList.contains("invisible") &&
+        btn3.classList.contains("invisible") &&
+        btn4.classList.contains("invisible") &&
+        btn5.classList.contains("invisible") &&
+        btn6.classList.contains("invisible");
 }
 
 function visibleAll() {
-    btn1.classList.remove("hide");
-    btn2.classList.remove("hide");
-    btn3.classList.remove("hide");
-    btn4.classList.remove("hide");
-    btn5.classList.remove("hide");
-    btn6.classList.remove("hide");
+    btn1.classList.remove("invisible");
+    btn2.classList.remove("invisible");
+    btn3.classList.remove("invisible");
+    btn4.classList.remove("invisible");
+    btn5.classList.remove("invisible");
+    btn6.classList.remove("invisible");
 }
 
 function selectButton(element) {
+    if(end) return;
     if(!started) startGame();
     if(parseInt(element.textContent) === sequence[selected]) {
         selected += 1;
     } else {
         changeBackground();
         visibleAll();
+        count_errors +=1;
         selected = 0;
         return;
     }
-    element.classList.add("hide");
+    element.classList.add("invisible");
     changeBackground(element.style.backgroundColor);
 
-    if(checkEndgame()) stopTimer();
+    if(checkEndgame()) setEndGame();
 }
 
 function changeBackground(color = WHITE) {
@@ -95,14 +108,28 @@ function changeBackground(color = WHITE) {
 
 function initTimer() {
     timer = setInterval(() => {
-        if(points <= 0) stopTimer();
-        points = points - 10;
+        if(points <= 0) setEndGame();
+        points = points - 20;
+        progress.style.width = (points * 100 / TIME_GAME) +"%";
         if(points >= 0) score.textContent = points;
     }, 10);
 }
 
-function stopTimer() {
+function setEndGame() {
+    end = true;
     clearInterval(timer);
+    errors.textContent = "Erros: "+count_errors;
+    changeContent();
+}
+
+function changeContent() {
+    if(end) {
+        game.classList.add("hide");
+        end_message.classList.remove("hide");
+    } else {
+        game.classList.remove("hide");
+        end_message.classList.add("hide");
+    }
 }
 
 Array.prototype.shuffle = () => sequence.sort(() => .5 - Math.random());
